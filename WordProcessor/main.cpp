c9aa320892357496b45d58b008304973e93f9483
Daniel Walsh <dangwalsh@gmail.com>
2013-11-15 09:35:58 -0500

    Initial Commit

---
+++ WordProcessor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WordProcessor
+//
+//  Created by Daniel Walsh on 11/15/13.
+//  Copyright (c) 2013 Daniel Walsh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
