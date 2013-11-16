213629ef4db4f29938127d6d706ede5fb1ab6dda
dangwalsh <dangwalsh@gmail.com>
2013-11-16 15:47:27 -0500

    clean up output formatting

--- WordProcessor/main.cpp
+++ WordProcessor/main.cpp
@@ -5,9 +5,9 @@
 //  Created by Daniel Walsh on 11/15/13.
 //  Copyright (c) 2013 Daniel Walsh. All rights reserved.
 //
-//#define MICROSOFT
-#define _DEBUG
-#define MAX 5
+#define MICROSOFT
+#define __DEBUG
+#define MAX 40
 
 #include <iostream>
 #include <list>
@@ -31,8 +31,8 @@ int main()
 	string phrase; 
 	size_t count = 0;
 
-	cout << "Begin typing:\n";
-	getline(cin, phrase);
+	cout << "Begin typing:\n\n";
+
 	while (count < MAX) {
 		getline(cin, phrase);
         if (phrase == "#") break;
@@ -40,67 +40,69 @@ int main()
 		++count;
 	}
 
+	cout << "\nThese are the lines in your document:\n" << endl;
 	print();
 
-	cout << "Enter a command:\n";
 	char comm; int start, end, target;  string str; string entry;
 	while (1) {
-        cout << "Enter a command: ";
+        cout << "\nEnter a command: ";
         getline(cin, entry);
         comm = entry[0];
 		switch(comm) {
             case 'I':
-                cout << "At line: ";
+				cout << "\nInsert phrase: ";
+				getline(cin, str);
+                cout << "\nAt line: ";
                 getline(cin, entry);
                 start = atoi(entry.c_str());
-                cout << "Insert phrase:";
-                getline(cin, str);
                 insertLine(start, str);
                 break;
             case 'A':
-                cout << "Append phrase: " << endl;
+                cout << "\nAppend phrase: ";
                 getline(cin, entry);
                 appendLine(entry);
                 break;
             case 'M':
-                cout << "Move lines: " << endl;
+                cout << "\nMove lines: ";
                 getline(cin, entry);
                 start = atoi(entry.c_str());
-                cout << "Through: " << endl;
+                cout << "\nThrough: ";
                 getline(cin, entry);
                 end = atoi(entry.c_str());
-                cout << "To line: " << endl;
+                cout << "\nTo line: ";
                 getline(cin, entry);
                 target = atoi(entry.c_str());
                 moveLine(start, end, target);
                 break;
             case 'F':
-                cout << "Find: " << endl;
+                cout << "\nFind: " << endl;
                 getline(cin, entry);
                 findLine(entry);
                 break;
             case 'D':
-                cout << "Delete line: ";
+                cout << "\nDelete line: ";
                 getline(cin, entry);
                 start = atoi(entry.c_str());
                 deleteLine(start);
                 break;
             case 'Q':
-                cout << "Exiting..." << endl;
+                cout << "\nExiting..." << endl;
                 break;
             default:
-                cout << "Command not supported!" << endl;
+                cout << "\nCommand not supported!\n" << endl;
                 break;
 		}
 
 		if (comm == 'Q') break;
-#ifdef _DEBUG
+#ifdef __DEBUG
+		cout << endl;
 		print();
 #endif
 	}
 
+	cout << "\nSorting phrases...\n" << endl;
 	sortLines();
-
+	cout << endl;
 #ifdef MICROSOFT
 	system("PAUSE");
 #endif
